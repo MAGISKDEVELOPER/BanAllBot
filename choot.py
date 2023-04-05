6fb94662179dce2d782330f07eb7ad88d48f7a33
MAGISKDEVELOPER <110710093+MAGISKDEVELOPER@users.noreply.github.com>
2023-04-05 17:13:44 +0530

    Update choot.py

--- choot.py
+++ choot.py
@@ -7,4 +7,4 @@ class Var:
     API_KEY = config("21124451", default=6, cast=int)
     API_HASH = config("d0c75e0e8ae1f79ddad10a033411f9ed", None)
     TOKEN = config("6246031074:AAHtUpc0r-ynm1KnmP56yvGMcWp7SWpBvRU", None)
-    OWNER_ID = list(map(int, getenv("6161624470").split()))
+    OWNER_ID = list(map(int, getenv("OWNER_ID").split()))
